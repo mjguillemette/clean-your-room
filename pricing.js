ce52fc1df77d92814081f4105001048e65ed98ee
Michael Guillemette <mike.guillemette@gmail.com>
2019-05-14 08:40:11 -0400

    Made changes based on your feedback and suggestions. Thanks!

--- pricing.js
+++ pricing.js
@@ -1,82 +1,85 @@
-let coverage = 0
-let cost = 0
-
-export function  calculateProductPrice(product, employee, selectedOptions){
-  let price = 0
-  const familyToCover = selectedOptions.familyMembersToCover
-
-  if (product.type != 'medical' && product.type != 'volLife' && product.type != 'ltd') {
-    throw new Error(`Unknown product type: ${product.type}`)
-  } 
-
-  price += medicalPricing(product, familyToCover)
-  price += voluntaryPricing(product, familyToCover, selectedOptions)
-  price += disabilityPricing(product, familyToCover, employee)
-
-  return parseInt(price * 100) / 100
-}
-
-function medicalPricing(product, familyToCover){
+function medicalPricing(costs, familyToCover){
   let medicalPrice = 0
 
-  if (product.type != 'medical') { return 0 }
-  for(var x in familyToCover){
-    let familyMember = familyToCover[x]
-    
-    medicalPrice += product.costs.find(cost => {
+  familyToCover.forEach( (familyMember) => {
+    medicalPrice += costs.find(cost => {
       return cost.role === familyMember
     }).price
-  }
+  })
+
   return medicalPrice
 }
 
-function voluntaryPricing(product, familyToCover, selectedOptions){
-  if (product.type != 'volLife') { return 0 }
-  let voluntaryPricing = 0
-  for(var x in familyToCover){
-    let familyMember = familyToCover[x]
-    console.log(familyMember)
+function voluntaryPricing(productCosts, familyToCover, selectedOptions){
 
+  let voluntaryPricing = 0
+  let coverage = 0
+  let cost = 0
+
+  familyToCover.forEach( (familyMember) => {
     coverage = selectedOptions.coverageLevel.find(coverage => {
       return coverage.role === familyMember
     }).coverage
 
-    cost = product.costs.find(cost => {
+    cost = productCosts.find(cost => {
       return cost.role === familyMember
     })
 
     voluntaryPricing += (coverage / cost.costDivisor) * cost.price
-  }
-
-  return employerContribution(product, voluntaryPricing)
+  })
+  
+  return  voluntaryPricing
 }
 
-function disabilityPricing(product, familyToCover, employee){
-  if (product.type != 'ltd') { return 0 }
+function disabilityPricing(productCosts, productCoverage, familyToCover, employee){
+
   let disabilityPrice = 0
-  for(var x in familyToCover){
-    let familyMember = familyToCover[x]
-    coverage = product.coverage.find(coverage => {
+  let coverage = 0
+  let cost = 0
+
+  familyToCover.forEach( (familyMember) => {
+    coverage = productCoverage.find(coverage => {
       return coverage.role === familyMember
     })
-    cost = product.costs.find(cost => {
+    cost = productCosts.find(cost => {
       return cost.role === familyMember
     })
 
     const salaryPercentage = coverage.percentage / 100
     disabilityPrice += ((employee.salary * salaryPercentage) / cost.costDivisor) * cost.price
-  }
+  })
   
   disabilityPrice = (disabilityPrice * 100) / 100
-  return employerContribution(product, disabilityPrice)
+  return disabilityPrice
 }
 
 
-function employerContribution(product, pricing){
-  if(product.employerContribution.mode === 'dollar'){
-    return pricing - product.employerContribution.contribution
+function employerContribution(employerContribution, pricing){
+  if(employerContribution.mode === 'dollar'){
+    return pricing - employerContribution.contribution
   } else { 
-    const dollarsOff = pricing * (product.employerContribution.contribution /100)
+    const dollarsOff = pricing * (employerContribution.contribution /100)
     return pricing - dollarsOff
   }
+}
+
+export function  calculateProductPrice(product, employee, selectedOptions){
+  let price = 0
+  const familyToCover = selectedOptions.familyMembersToCover
+
+  switch (product.type) {
+    case ('medical'):
+      price += medicalPricing(product.costs, familyToCover)
+      break
+    case ('volLife'):
+      price += employerContribution(product.employerContribution, voluntaryPricing(product.costs, familyToCover, selectedOptions))
+      break
+    case ('ltd'):
+      price += employerContribution(product.employerContribution, disabilityPricing(product.costs, product.coverage, familyToCover, employee))
+      break
+    default:
+      throw new Error(`Unknown product type: ${product.type}`)
+  }
+
+  return parseInt(price * 100) / 100
 }
